41eb2b7cfda0b7c2cb342cc4fe3dfc42288d9a25
Krai53n <greasha46@gmail.com>
2021-03-13 21:00:52 +0300

    add_checking_extension_on_Python_file

--- pymin/find_mds.py
+++ pymin/find_mds.py
@@ -11,11 +11,13 @@ IGNORE_DIRS = [".git"]
 
 def dir_jogging(path):
     """
-    Give information about directory. For example give
-    adress to files, other directories and files
-    Return information as dictionary
+    Argument path is path to directory.
+    Function return:
+        0) files that recieves from directory recursively.
+        1) name of file from 0 point
     """
-    data = []
+    pth = []
+    file_names = []
     for root, dirs, files in walk(path):
         # skip directory with names in IGNORE_DIRS
         cont = False
@@ -27,10 +29,53 @@ def dir_jogging(path):
             
         if files != 0:
             for file in files:
-                data.append(os_path.join(root, file))
-    return data
+                pth.append(os_path.join(root, file))
+                file_names.append(file)
+    return pth, file_names
+
+def check_file(path, file):
+    """
+    Check it is Python file or not
+    Arguments:
+        path - path to file with name of file
+        file - file
+    """
+    if file[-3:] == ".py":
+        return 1
+    if "." not in file:
+        py_file = False
+        py_string = "#!/bin/python"
+        long_comment = False
+        with open(path) as f:
+            lines = f.readlines()
+
+        for line in lines:
+            # avoid comments
+            if "//" in line:
+                continue
+            # NOTE: with condition that situated under can be problems
+            if (py_string[0] in line[0]) and (py_string[1] not in line[1]):
+                continue
+            if ("'" == line[0]) and ("'" == line[-1]):
+                continue
+            if ('"' == line[0]) and ('"' == line[-1]):
+                continue
+            if (line.count("'''") == 2) or (line.count('"""') == 2):
+                continue
+            if (line.count("'''") == 1) or (line.count('"""') == 1):
+                if long_comment:
+                    long_comment = False
+                if not long_comment:
+                    long_comment = True
+            if (long_comment == False) and (py_string in line):
+                return 1
+
 
 if __name__ == "__main__":
     from os import getcwd
     path = getcwd()
-    [print(i) for i in dir_jogging(path)]
+    data = dir_jogging(path)
+    # [print(data[0][i], "\t"*3, data[1][i]) for i in range(len(data[0]))]
+    path_file, file = data[0][4], data[1][4]
+    print(path_file, file)
+    print(check_file(path_file, file))
---
+++ test/str.py
@@ -0,0 +1,10 @@
+line = "''' asdasdasd '''"
+# line = '""" asdasd asfsdf """'
+
+# if ("#" in line) and (py_string not in line):
+#     continue
+# if ("'" == line[0]) and ("'" == line[-1]):
+#     continue
+# if ('"' == line[0]) and ('"' == line[-1]):
+#     continue
+print(line.count("'''"))
