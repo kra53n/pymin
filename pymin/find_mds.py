b74861a773502bc2f6b7996361f0c27f0cd9efc0
Krai53n <greasha46@gmail.com>
2021-03-14 18:31:13 +0300

    (<-_-><-_->)

--- pymin/find_mds.py
+++ pymin/find_mds.py
@@ -33,42 +33,52 @@ def dir_jogging(path):
                 file_names.append(file)
     return pth, file_names
 
+def find_string_in_file(path, find):
+    """
+    Arguments:
+        0) path - path to file
+        1) find - string that will be searched.
+        Searching avoid various comments
+    Return line that have `find` value
+    """
+    # help avoid long comments
+    long_comment = False
+    with open(path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        # avoid comments
+        if "//" in line:
+            continue
+        # NOTE: with condition that situated under can be problems
+        if (find[0] in line[0]) and (find[1] not in line[1]):
+            continue
+        if ("'" == line[0]) and ("'" == line[-1]):
+            continue
+        if ('"' == line[0]) and ('"' == line[-1]):
+            continue
+        if (line.count("'''") == 2) or (line.count('"""') == 2):
+            continue
+        if (line.count("'''") == 1) or (line.count('"""') == 1):
+            if long_comment:
+                long_comment = False
+            if not long_comment:
+                long_comment = True
+        if (long_comment == False) and (find in line):
+            return line
+
 def check_file(path, file):
     """
     Check it is Python file or not
     Arguments:
         path - path to file with name of file
-        file - file
+        file - name of file
     """
     if file[-3:] == ".py":
         return 1
     if "." not in file:
-        py_file = False
-        py_string = "#!/bin/python"
-        long_comment = False
-        with open(path) as f:
-            lines = f.readlines()
-
-        for line in lines:
-            # avoid comments
-            if "//" in line:
-                continue
-            # NOTE: with condition that situated under can be problems
-            if (py_string[0] in line[0]) and (py_string[1] not in line[1]):
-                continue
-            if ("'" == line[0]) and ("'" == line[-1]):
-                continue
-            if ('"' == line[0]) and ('"' == line[-1]):
-                continue
-            if (line.count("'''") == 2) or (line.count('"""') == 2):
-                continue
-            if (line.count("'''") == 1) or (line.count('"""') == 1):
-                if long_comment:
-                    long_comment = False
-                if not long_comment:
-                    long_comment = True
-            if (long_comment == False) and (py_string in line):
-                return 1
+        if find_string_in_file(path, "#!/bin/python"):
+            return 1
 
 def get_py_files(path):
     """
@@ -84,7 +94,18 @@ def get_py_files(path):
             py_path_to_files.append(path_files[i])
     return py_path_to_files
 
+def find_mds(path):
+    """
+    Argument path - path to directory
+    """
+    paths = get_py_files(path)
+    strings = [find_string_in_file(p, "import") for p in paths]
+    return strings
+
 if __name__ == "__main__":
     from os import getcwd
-    path = getcwd()
-    [print(i) for i in get_py_files(path)]
+    # path = getcwd()
+    path = "/home/kra53n/Рабочий стол/getgit"
+    # [print(i) for i in get_py_files(path)]
+    md = find_mds(path)
+    [print(i) for i in find_mds(path)]
