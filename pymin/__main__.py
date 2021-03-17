546ef2615ff13cd3eb644fd0b79820ade2b24a4f
Krai53n <greasha46@gmail.com>
2021-03-17 10:03:37 +0300

    (×﹏×)

--- pymin/__main__.py
+++ pymin/__main__.py
@@ -1,7 +1,19 @@
 from __init__ import return_mds
+from os import getcwd
 
+
+def print_mds(path):
+    mds = return_mds(path)
+    keys = mds.keys()
+    message = ""
+    for key in keys:
+        message += "\n" + key.upper() + "\n"
+        for i in range(len(mds[key])):
+            message += "\t" + mds[key][i]
+            if i != len(mds[key])-1:
+                message += "\n"
+    print(message[1:])
 
 
 if __name__ == "__main__":
-    from os import getcwd
-    print(return_mds(getcwd()))
+    print_mds(getcwd())
