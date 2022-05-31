980f7031f4b9d76159f98a37ac0b3ca295b5a86c
kra53n <59062496+kra53n@users.noreply.github.com>
2022-05-31 20:20:18 +0300

    Update __main__.py

--- pymin/__main__.py
+++ pymin/__main__.py
@@ -4,12 +4,8 @@ from find_mds import find_mds, dir_jogging
 from analyze import analyze_mds
 
 
-def return_mds(path):
-    return analyze_mds(find_mds(path), dir_jogging(path)[1])
-
-
 def print_mds(path):
-    mds = return_mds(path)
+    mds = analyze_mds(find_mds(path), dir_jogging(path)[1])
     sep = '\n\t'
     message = '\n'.join([f"{module_type.upper()}\n\t{sep.join((v for v in values))}"
                          for module_type, values in mds.items()])
